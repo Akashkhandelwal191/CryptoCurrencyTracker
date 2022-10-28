a3436d1f9f20bd69ae00a3d6d4999a72b2376ff1
Akash khandelwal <skykhandelwal1999@gmail.com>
2022-10-28 18:57:13 +0530

    Version 1

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,41 @@
+import { Container, Typography } from '@mui/material';
+import React from 'react';
+import "./Banner.css";
+import Carousel from '../Carousel/Carousel';
+
+
+//Banner Component After Header
+const Banner = () => {
+  return (
+    <div className='Banner'> 
+       <Container className="BannerContent">
+           <div className='BannerTagLine'>
+          
+             <Typography variant='h2' 
+             style={{
+
+                fontWeight:"bold",
+                marginBottom:15,
+                fontFamily:"Monserrat",       
+
+             }}>Crypto Tracker</Typography>
+             
+             <Typography
+            variant="subtitle2"
+            style={{
+              color: "darkgrey",
+              textTransform: "capitalize",
+              fontFamily: "Montserrat",
+            }}
+          >
+            Get all the Info regarding your favorite Crypto Currency
+          </Typography>
+           </div>
+   
+             <Carousel />     
+       </Container>
+    </div>
+  )
+ }
+ 
+export default Banner;
---
+++ src/components/CoinsTable/CoinsTable.js
@@ -0,0 +1,187 @@
+import axios from 'axios';
+import React, { useState,useEffect } from 'react';
+import { CoinList } from '../../config/api';
+import { CryptoState } from '../../CryptoContext';
+import {createTheme,TableContainer,TextField,ThemeProvider, Typography,Table, LinearProgress, TableHead,TableBody,TableCell,TableRow,Paper} from '@mui/material';
+import { Container } from '@mui/system';
+import { numberWithCommas } from '../../utilities/utilities';
+import { useNavigate } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import "./CoinsTable.css";
+
+
+const CoinsTable = () => {
+ 
+  const [Coins,SetCoins] = useState([]);
+  const [Loading,SetLoading] = useState(false);
+  const [Search,SetSearch] = useState("");
+  const [page, setPage] = useState(1);
+
+  const { currency, symbol } = CryptoState();
+  const navigate = useNavigate();
+
+  const darkTheme = createTheme({
+    palette: {
+      primary:{
+        main: "#fff",
+      },
+      mode:"dark"  
+    },
+  });
+ 
+ 
+
+   const fetchCoins = async()=>{
+      SetLoading(true);
+      const {data} = await axios.get(CoinList(currency));
+      SetCoins(data); 
+      SetLoading(false);
+      console.log(Coins);
+   }
+  
+   useEffect(()=>{
+      
+       fetchCoins();
+         // eslint-disable-next-line
+   },[currency])
+
+   const handleSearch = () => {
+    return Coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(Search) ||
+        coin.symbol.toLowerCase().includes(Search)
+    );
+  };
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+   
+       <Container style={{textAlign:"center"}}>
+          
+          <Typography variant='h4' style={{margin:18, fontFamily:"Monserrat"}}>
+             Cryptocurrency Prices by Market Cap
+          </Typography>
+   
+          <TextField
+          label="Search For a Crypto Currency.."
+          variant="outlined"
+          style={{ marginBottom: 20, width: "100%" }}
+          onChange={(e) => SetSearch(e.target.value)}
+        />
+
+
+<TableContainer component={Paper}>
+          {Loading ? (
+            <LinearProgress style={{ backgroundColor: "gold" }} />
+          ) : (
+            <Table aria-label="simple table">
+              <TableHead style={{ backgroundColor: "#EEBC1D" }}>
+                <TableRow>
+                  {["Coin", "Price", "24h Change", "Market Cap"].map((head) => (
+                    <TableCell
+                      style={{
+                        color: "black",
+                        fontWeight: "700",
+                        fontFamily: "Montserrat",
+                      }}
+                      key={head}
+                      align={head === "Coin" ? "left" : "right"}
+                    >
+                      {head}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {handleSearch()
+                  .slice((page - 1) * 10, (page - 1) * 10 + 10)
+                  .map((row) => {
+                    const profit = row.price_change_percentage_24h > 0;
+                    return (
+                      <TableRow
+                        onClick={() => navigate(`/coins/${row.id}`)}
+                        className="row"
+                        key={row.name}
+                      >
+                        <TableCell
+                          component="th"
+                          scope="row"
+                          style={{
+                            display: "flex",
+                            gap: 15,
+                          }}
+                        >
+                          <img
+                            src={row?.image}
+                            alt={row.name}
+                            height="50"
+                            style={{ marginBottom: 10 }}
+                          />
+                          <div
+                            style={{ display: "flex", flexDirection: "column" }}
+                          >
+                            <span
+                              style={{
+                                textTransform: "uppercase",
+                                fontSize: 22,
+                              }}
+                            >
+                              {row.symbol}
+                            </span>
+                            <span style={{ color: "darkgrey" }}>
+                              {row.name}
+                            </span>
+                          </div>
+                        </TableCell>
+                        <TableCell align="right">
+                          {symbol}{" "}
+                          {numberWithCommas(row.current_price.toFixed(2))}
+                        </TableCell>
+                        <TableCell
+                          align="right"
+                          style={{
+                            color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+                            fontWeight: 500,
+                          }}
+                        >
+                          {profit && "+"}
+                          {row.price_change_percentage_24h.toFixed(2)}%
+                        </TableCell>
+                        <TableCell align="right">
+                          {symbol}{" "}
+                          {numberWithCommas(
+                            row.market_cap.toString().slice(0, -6)
+                          )}
+                          M
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+              </TableBody>
+            </Table>
+          )}
+        </TableContainer>
+   
+       </Container>
+    
+       <Pagination
+          count={Number((handleSearch()?.length / 10).toFixed(0))}
+          style={{
+            padding: 20,
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+          }}
+          classes={{ul:"pagination"}}
+          onChange={(_, value) => {
+            setPage(value);
+            window.scroll(0, 450);
+          }}
+        />
+
+    </ThemeProvider>
+  )
+}
+
+export default CoinsTable;
